70e6d3b5b4b674283d8d6d2f93aedff62f2b691c
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2026-04-20 22:08:52 +0200

    drop has lib

--- src/components/wp/Media.jsx
+++ src/components/wp/Media.jsx
@@ -1,5 +1,4 @@
 import { useQuery } from '@tanstack/react-query';
-import has from 'has';
 
 import Loading from '../general/Loading';
 
@@ -21,21 +20,9 @@ function Media({ alt, id, fallback }) {
         return <Loading small error={error} />;
     }
 
-    const src =
-        data && has(data, 'source_url')
-            ? data.source_url
-            : fallback || fallbackImg;
+    const src = data?.source_url ?? fallback ?? fallbackImg;
 
-    return (
-        <img
-            src={src}
-            alt={
-                data && has(data, 'alt_text') && data.alt_text
-                    ? data.alt_text
-                    : alt
-            }
-        />
-    );
+    return <img src={src} alt={data?.alt_text ?? alt} />;
 }
 
 export default Media;
