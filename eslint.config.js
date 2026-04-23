49c7457928e1a4b308b80537abd7cbfd3607e06a
Dalibor Hrtanek <dalibor.hrtanek@gmail.com>
2026-04-23 23:56:24 +0200

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,60 @@
 // eslint.config.js
 import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 import prettierPlugin from 'eslint-plugin-prettier';
 import reactPlugin from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
 
 export default [
-  js.configs.recommended,
-  {
-    plugins: { prettier: prettierPlugin },
-  },
-  {
-    plugins: {
-      react: reactPlugin,
+    js.configs.recommended,
+    eslintConfigPrettier,
+    {
+        files: ['**/*.{js,mjs,cjs,jsx,mjsx,ts,tsx,mtsx}'],
+        ...jsxA11y.flatConfigs.recommended,
+        rules: {
+            ...jsxA11y.flatConfigs.recommended.rules,
+            'jsx-a11y/alt-text': [0],
+        },
     },
-    rules: {
-      ...reactPlugin.configs['jsx-runtime'].rules,
-      'jsx-a11y/alt-text': [0],
-      'react/jsx-no-useless-fragment': [0],
-      'react/jsx-props-no-spreading': [1, { html: 'ignore' }],
-      'react/prop-types': [0], // TODO: enable this
-      'react/react-in-jsx-scope': 'off',
+    {
+        plugins: { prettier: prettierPlugin },
     },
-  },
-  {
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+    reactPlugin.configs.flat.recommended,
+    reactPlugin.configs.flat['jsx-runtime'],
+    {
+        rules: {
+            'react/jsx-no-useless-fragment': [0],
+            'react/jsx-props-no-spreading': [1, { html: 'ignore' }],
+        },
     },
-    rules: {
-      'linebreak-style': ['error', 'unix'],
-      'no-irregular-whitespace': [
-        1,
-        { skipStrings: true, skipTemplates: true, skipJSXText: true },
-      ],
+    {
+        plugins: {
+            'react-hooks': reactHooks,
+        },
+        settings: { react: { version: 'detect' } },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            'react/react-in-jsx-scope': 'off',
+            'react/prop-types': 'off',
+        },
     },
-  },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.serviceworker,
+                ...globals.browser,
+            },
+            ecmaVersion: 'latest',
+            sourceType: 'module',
+        },
+        rules: {
+            'linebreak-style': ['error', 'unix'],
+            'no-irregular-whitespace': [
+                1,
+                { skipStrings: true, skipTemplates: true, skipJSXText: true },
+            ],
+        },
+    },
 ];
